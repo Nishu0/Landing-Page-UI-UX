54be735bc722b4d3ade38c3986c26320b5098f47
Nisarg Thakkar <89217455+Nishu0@users.noreply.github.com>
2024-01-26 23:45:37 +0530

    :boom: Features

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -15,7 +15,7 @@ export function Hero(){
                 Start monitoring your website like a pro
                 </h1>
                 <p className="text-center pt-6 text-[#36485C]lg:text-[18px] lg:leading-7">
-                Get a bird's eye view with our customizable dashboard. Stay on top of things! Revamp your work process with our game-changing feature. Boost productivity and efficiency!
+                Get a bird eye view with our customizable dashboard. Stay on top of things! Revamp your work process with our game-changing feature. Boost productivity and efficiency!
                 </p>
                 <div className="flex w-full pt-8 justify-center gap-x-6">
                     <button className="bg-[#4328EB] w-1/2 py-4 px-8 text-white rounded-[4px] lg:w-fit">Try for Free</button>
