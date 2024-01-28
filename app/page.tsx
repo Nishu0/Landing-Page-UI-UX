fdb13afc55ec6e05d062a33fb80d09835e0454ad
Nisarg Thakkar <89217455+Nishu0@users.noreply.github.com>
2024-01-28 23:59:58 +0530

    :bento: Completed

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 import { Navbar } from "@/components/Navbar";
 import { Hero } from "@/components/Hero";
 import { Features } from "@/components/Features";
+import { Faq } from "@/components/FAQ";
+import { Cta } from "@/components/CTA";
+import { Pricing } from "@/components/Plan";
+import { Footer } from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -10,6 +14,10 @@ export default function Home() {
     <Hero />
     <div className="px-[20px] lg:container lg:px-20 mx-auto">
     <Features />
+    <Faq />
+    <Pricing />
+    <Cta />
+    <Footer />
     </div>
     </>
   );
